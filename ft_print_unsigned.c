604d40d013e3319d8a8b2703022f2eaf6271f342
JayuSC2 <134278095+JayuSC2@users.noreply.github.com>
2023-10-10 07:08:11 +0000

    fixed unsigned

--- ft_print_unsigned.c
+++ ft_print_unsigned.c
@@ -1,40 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_unsigned.c                                   :+:      :+:    :+:   */
+/*   ft_print_unsigned.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 10:48:40 by codespace         #+#    #+#             */
-/*   Updated: 2023/10/06 11:36:49 by codespace        ###   ########.fr       */
+/*   Updated: 2023/10/10 07:07:58 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "printf.h"
 
-void	ft_print_unsigned (unsigned int n, int fd)
+void	ft_print_unsigned (unsigned int n)
 {
 	char	ch;
 
-	if (n < 0)
-	{
-		n *= -1;
-	}
 	if (n >= 0 && n < 10)
 	{
 		ch = n + '0';
-		write(fd, &ch, 1);
+		write(1, &ch, 1);
 	}
-	if (n >= 10 && n <= 2147483647)
+	if (n >= 10 && n <= 4294967295)
 	{
-		ft_print_unsigned(n / 10, fd);
-		ft_print_unsigned(n % 10, fd);
+		ft_print_unsigned(n / 10);
+		ft_print_unsigned(n % 10);
 	}
 }
 
 #include <stdio.h>
 int main(void)
 {
-    ft_print_unsigned(-4, 1);
+    ft_print_unsigned(-4);
+	printf("%u",-4);
 }
